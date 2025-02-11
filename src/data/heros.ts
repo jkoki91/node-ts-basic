042bb06d83ae33514bf92c35a88164ffe98e515f
jkoki91 <>
2025-02-11 13:21:31 +0100

    first commit

---
+++ src/data/heros.ts
@@ -0,0 +1,24 @@
+
+interface Hero {
+    id: number;
+    name: string;
+    owner: string;
+}
+
+export const heroes: Hero[] = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC',
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel',
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC',
+    },
+]
---
+++ src/services/hero.service.ts
@@ -0,0 +1,7 @@
+import { heroes } from "../data/heros";
+
+
+
+export const findHeroById = (id: number) => {
+    return heroes.find(hero => hero.id === id);
+}
